e267a776fe8110bbc238004352edff9ea85bf626
CK Ooi <choonkhiing97@gmail.com>
2019-04-01 09:05:39 +0800

    Merge

--- database/migrations/2019_03_24_111211_alter_tasks_table.php
+++ database/migrations/2019_03_24_111211_alter_tasks_table.php
@@ -15,6 +15,7 @@ class AlterTasksTable extends Migration
     {
         Schema::table('tasks', function (Blueprint $table) {
             $table->datetime('start_date')->nullable()->after('priority');
+            $table->datetime('min_duration')->nullable()->after('priority');
         });
     }
 
