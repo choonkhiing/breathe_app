b865724861c2c02d315a428ef1ed1bd8587261f9
Andrew Khor <andrews.kkg@gmail.com>
2019-04-29 22:49:51 +0800

    Collection update and delete

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Redirect;
+use Auth;
 
 use \App\Collection;
+use \App\Task;
 
 class CollectionController extends Controller
 {
@@ -16,7 +18,8 @@ class CollectionController extends Controller
      */
     public function index()
     {
-        return view("user.collections.index");
+        $collections = Collection::where("user_id", Auth::user()->id)->get();
+        return view("user.collections.index", compact("collections"));
     }
 
     /**
@@ -55,7 +58,12 @@ class CollectionController extends Controller
      */
     public function show($id)
     {
-        //
+        try {
+            $collection = Collection::findOrFail($id);
+            return response()->json(['success' => true, 'data' => $collection]);
+        } catch (Exception $e) {
+            return response()->json(['success' => false, 'msg' => $e->getMessage()]);
+        }
     }
 
     /**
@@ -78,7 +86,18 @@ class CollectionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $collection = Collection::find($id);
+            $collection->title = $request->title;
+            $collection->description = $request->description;
+            $collection->save();
+            $this->successMessage("Successfully edited collection.");
+            return redirect::back();
+        } catch (Exception $e) {
+            $this->errorMessage("Unable to edit collection. " . $e->getMessage());
+            return redirect::back();
+        }
+
     }
 
     /**
@@ -89,6 +108,9 @@ class CollectionController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Remove collections from tasks
+        $task = Task::where("collection_id", $id)->update(['collection_id' => null]);
+        $collection = Collection::find($id)->delete();
+        return response()->json(['success' => true, 'msg' => "Successfully deleted collection."]);
     }
 }
--- resources/views/user/collections/index.blade.php
+++ resources/views/user/collections/index.blade.php
@@ -3,10 +3,11 @@
 @section("header", "Collections")
 
 @section("content")
-<form action="{{ action('CollectionController@store') }}" method="POST" autocomplete="off">
-	@csrf
-	<div class="row">
-		<div class="col-md-6 offset-md-3">
+
+<div class="row">
+	<div class="col-md-6">
+		<form action="{{ action('CollectionController@store') }}" data-parsley-validate="true" method="POST" autocomplete="off">
+			@csrf
 			<div class="panel">
 				<div class="panel-heading">
 					<h4 class="panel-title">Create new collection</h4>
@@ -14,7 +15,7 @@
 				<div class="panel-body">
 					<div class="form-group">
 						<label class="title">Title</label>
-						<input name="title" type="text" class="form-control form-control-lg" placeholder="Enter Title">
+						<input name="title" type="text" required="" class="form-control form-control-lg" placeholder="Enter Title">
 					</div>
 					<div class="form-group">
 						<label class="title">Description (Optional)</label>
@@ -25,7 +26,121 @@
 					<button type="submit" class="btn btn-primary btn-action pull-right">Create collection</button>
 				</div>
 			</div>
+		</form>
+	</div>
+
+	<div class="col-md-6">
+		<div class="table-responsive">
+			<table class="table table-bordered table-striped bg-white">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th width="1"></th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($collections AS $cl)
+					<tr data-cl="{{ $cl->id }}">
+						<td>{{ $cl->title }}<br>{{ $cl->description }}</td>
+						<td class="nowrap">
+							<button type="button" class="btn btn-success btn-edit btn-action">Edit</button>
+							<button type="button" class="btn btn-danger btn-delete btn-action">Delete</button>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
 		</div>
 	</div>
-</form>
+</div>
+
+<div class="modal" id="modalEdit" tabindex="-1" role="dialog" aria-labelledby="taskDetailTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content pop-up">
+			<form id="form_edit" data-parsley-validate="true" method="POST">
+				@method("PATCH")
+				@csrf
+				<div class="modal-header">
+					<h4 class="modal-title">Edit Collection</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label class="title">Title</label>
+						<input name="title" type="text" class="form-control form-control-lg" placeholder="Enter Title">
+					</div>
+					<div class="form-group">
+						<label class="title">Description (Optional)</label>
+						<textarea class="form-control form-control-lg" rows="10" name="description" placeholder="Enter Description"></textarea>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default btn-action" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary btn-action">Udpdate</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+@stop
+
+@section("page_script")
+<script type="text/javascript">
+	$(function(){
+		$(".btn-delete").click(function(){
+
+			var $tr = $(this).closest("tr");
+			var collection_id = $tr.attr('data-cl');
+
+			Swal.fire({
+				title: 'Are you sure?',
+				text: "Deleted collection cannot be recovered.",
+				type: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				confirmButtonText: 'Yes, delete it!'
+			}).then((result) => {
+				if (result.value) {
+					$.ajax({
+						url: "/collections/" + collection_id,
+						method: "DELETE",
+						data: {
+							_token: '{{ csrf_token() }}',
+						},
+						success: function(response){
+							if(response.success){
+								notyf.success(response.msg);
+								$tr.remove();
+							}
+						}
+					})
+				}
+			})
+		});
+
+		$(".btn-edit").click(function(){
+			var $tr = $(this).closest("tr");
+			var collection_id = $tr.attr('data-cl');
+
+			$.get("/collections/" + collection_id, function(response){
+				console.log(response);
+				var form = $("#form_edit");
+				var cl = response.data;
+				form.find("input[name='title']").val(cl.title);
+				form.find("textarea[name='description']").val(cl.description);
+				form.find("input[name='cl_id']").val(cl.id);
+
+				form.attr("action", "/collections/" + cl.id);
+
+				$("#modalEdit").modal("toggle");
+			});
+		});
+
+
+	});
+</script>
 @stop
